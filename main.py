66b339c1bf13904003f22bbd03226ca188c8a0bf
Anshu Sinha <anshujuly2@gmail.com>
2023-04-03 09:35:58 +0530

    modify main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 """imported Screen class from the turtle module"""
+import time
 from turtle import Screen
 
 """created screen object"""
@@ -6,6 +7,15 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.title("Turtle crossing game")
 screen.bgcolor("white")
+screen.tracer(0)
+
+"""variable to hold the game's running condition"""
+isGameOn = True
+
+"""until the game is on, the loop will run"""
+while isGameOn:
+    screen.update()
+    time.sleep(0.08)
 
 """screen will exit on click"""
 screen.exitonclick()
